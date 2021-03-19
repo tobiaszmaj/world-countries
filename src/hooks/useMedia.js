fd910d8fe69b5c5a165631eb8bea5f8827f1d502
Tobiasz <tobiaszmaj1@gmail.com>
2021-03-19 16:21:10 +0100

    Fixed useMedia SSR bug

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const useMedia = query => {
-  const [matches, setMatches] = useState(true);
+  const [matches, setMatches] = useState(false);
 
   useEffect(() => {
     const media = window.matchMedia(query);
